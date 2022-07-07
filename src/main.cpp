ded03ed33611b43f424e2790a48e282a884d7693
Pranshu Vyas <79853965+v-Pranshu@users.noreply.github.com>
2022-07-07 14:40:00 +0530

    Added solveTruss function

--- src/main.cpp
+++ src/main.cpp
@@ -66,6 +66,7 @@ class Forces {
 void calculateReactions(Forces force[numForces], Joints joint[numJoints], Supports support[numSupports]);
 void member_forces(Joints joint[numJoints], Supports support[numSupports], Members member[numMembers], Forces force[numForces], int j, vector <int> deter, vector <int> undeter);
 void gauss_jordan(double A[nDim][nDim], double B[nDim][1], double F[nDim][1]);
+void solveTruss(Forces force[numForces], Joints joint[numJoints], Supports support[numSupports], Members member[numMembers]);
 
 int main()
 {
@@ -164,6 +165,87 @@ int main()
         member[i].length = sqrt(x1 + y1);
     }
 
+    solveTruss(force, joint, support, member);
+}
+
+// For now calculates only orthogonal reactions for a single external force applied on the truss
+void calculateReactions(Forces force[numForces], Joints joint[numJoints], Supports support[numSupports]){
+
+    double netExtForce;
+
+    for(int i = 0; i<numForces; i++){
+
+        support[1].reaction_force.y_comp += -force[i].components.y_comp*(force[i].x - joint[support[0].joint_index].x_coord)/(joint[support[1].joint_index].x_coord - joint[support[0].joint_index].x_coord);
+
+        netExtForce += force[i].components.y_comp;
+    }
+
+    support[0].reaction_force.y_comp = -netExtForce - support[1].reaction_force.y_comp;
+
+}
+
+void member_forces(Joints joint[numJoints], Supports support[numSupports], Members member[numMembers], Forces force[numForces],  int j, vector <int> deter, vector <int> undeter){      //j = index of joint at which forces are to be calculated
+
+    //Calculate forces of members passing through joint with 1st support
+
+    double B[2][1] = {{0}, {0}};
+    double A[2][2];
+    double F[2][1] = {{0}, {0}};
+
+    if(joint[j].isSupport){
+        B[0][0] = -support[joint[j].supInd].reaction_force.x_comp;
+        B[1][0] = -support[joint[j].supInd].reaction_force.y_comp;
+    }
+
+
+    else{
+
+        for(int i = 0; i < numForces; i++){
+            if(force[i].x == joint[j].x_coord && force[i].y == joint[j].y_coord){
+                B[0][0] += -(force[i].components.x_comp);
+                B[1][0] += -(force[i].components.y_comp);
+            }
+        }
+
+        for(int i = 0; i < deter.size(); i++){
+            B[0][0] += -(member[deter[i]].force.x);
+            B[1][0] += -(member[deter[i]].force.y);
+
+        }
+    }
+
+
+
+    for(int i = 0; i<undeter.size(); i++){
+
+        if (member[undeter[i]].end_point != j){
+            member[undeter[i]].away_point = member[undeter[i]].end_point;
+
+        }
+
+        else{
+            member[undeter[i]].away_point = member[undeter[i]].start_point;
+        }
+
+        A[0][i] += (joint[member[undeter[i]].away_point].x_coord - joint[j].x_coord)/(member[undeter[i]].length);
+        A[1][i] += (joint[member[undeter[i]].away_point].y_coord - joint[j].y_coord)/(member[undeter[i]].length);
+
+    }
+
+    gauss_jordan(A, B, F);
+
+    for(int i = 0; i<undeter.size(); i++){
+        member[undeter[i]].force.x = F[i][0]*A[0][i];
+        member[undeter[i]].force.netForce = F[i][0];
+        member[undeter[i]].force.y = F[i][0]*A[1][i];
+        member[undeter[i]].force.done = true;
+    }   
+}
+
+void solveTruss(Forces force[numForces], Joints joint[numJoints], Supports support[numSupports], Members member[numMembers]){
+    
+    //Calculate reaction forces by the support
+
     calculateReactions(force, joint, support);
 
     cout << "Reaction Force 2: " << support[1].reaction_force.y_comp << "N" << endl;
@@ -245,153 +327,10 @@ int main()
         cout << "ERROR";
     }
 
-
     for(int i = 0; i<numMembers; i++){
         cout << "member " << i <<  " : " << member[i].force.netForce << "N" << endl;
     }
-
 }
-
-// For now calculates only orthogonal reactions for a single external force applied on the truss
-void calculateReactions(Forces force[numForces], Joints joint[numJoints], Supports support[numSupports]){
-
-    double netExtForce;
-
-    for(int i = 0; i<numForces; i++){
-
-        support[1].reaction_force.y_comp += -force[i].components.y_comp*(force[i].x - joint[support[0].joint_index].x_coord)/(joint[support[1].joint_index].x_coord - joint[support[0].joint_index].x_coord);
-
-        netExtForce += force[i].components.y_comp;
-    }
-
-    support[0].reaction_force.y_comp = -netExtForce - support[1].reaction_force.y_comp;
-
-}
-
-
-void member_forces(Joints joint[numJoints], Supports support[numSupports], Members member[numMembers], Forces force[numForces],  int j, vector <int> deter, vector <int> undeter){      //j = index of joint at which forces are to be calculated
-
-    //Calculate forces of members passing through joint with 1st support
-
-    double B[2][1] = {{0}, {0}};
-    double A[2][2];
-    double F[2][1] = {{0}, {0}};
-
-    if(joint[j].isSupport){
-        B[0][0] = -support[joint[j].supInd].reaction_force.x_comp;
-        B[1][0] = -support[joint[j].supInd].reaction_force.y_comp;
-    }
-
-
-    else{
-
-        for(int i = 0; i < numForces; i++){
-            if(force[i].x == joint[j].x_coord && force[i].y == joint[j].y_coord){
-                B[0][0] += -(force[i].components.x_comp);
-                B[1][0] += -(force[i].components.y_comp);
-            }
-        }
-
-        for(int i = 0; i < deter.size(); i++){
-            B[0][0] += -(member[deter[i]].force.x);
-            B[1][0] += -(member[deter[i]].force.y);
-
-        }
-    }
-
-
-
-    for(int i = 0; i<undeter.size(); i++){
-
-        if (member[undeter[i]].end_point != j){
-            member[undeter[i]].away_point = member[undeter[i]].end_point;
-
-        }
-
-        else{
-            member[undeter[i]].away_point = member[undeter[i]].start_point;
-        }
-
-        A[0][i] += (joint[member[undeter[i]].away_point].x_coord - joint[j].x_coord)/(member[undeter[i]].length);
-        A[1][i] += (joint[member[undeter[i]].away_point].y_coord - joint[j].y_coord)/(member[undeter[i]].length);
-
-    }
-
-    gauss_jordan(A, B, F);
-
-    for(int i = 0; i<undeter.size(); i++){
-        member[undeter[i]].force.x = F[i][0]*A[0][i];
-        member[undeter[i]].force.netForce = F[i][0];
-        member[undeter[i]].force.y = F[i][0]*A[1][i];
-        member[undeter[i]].force.done = true;
-    }   
-}
-
-
-void solveTruss(){
-    Forces force[numForces];
-    Joints joint[numJoints];
-    Supports support[numSupports];
-    Members member[numMembers];
-
-
-    //Calculate reaction forces by the support
-
-    calculateReactions(force, joint, support);
-
-    vector <int> jointSolved;
-    vector <int> jointToBe_Solved;
-
-    for(int i = 0; i<numJoints; i++){
-        jointToBe_Solved.push_back(i);
-    }
-
-
-
-    while(jointToBe_Solved.size()!=0){
-
-        for(int j = 0; j<jointToBe_Solved.size(); j++){
-
-            vector <int> Member_ind;        //Indices of members passing through joint
-            vector <int> deter;             // Vector to store indices of members that pass through joint no. j whose forces have been determined
-            vector <int> undeter;
-
-
-            //On Given joint number check index of members passing through
-
-            for (int i = 0; i<numMembers; i++){
-                if (member[i].start_point == j || member[i].end_point == j){
-                    Member_ind.push_back(i);
-                }
-            }
-
-            //Initially member.forces = -1.0 (for all members for which forces have not yet been determined)
-
-            for (int i = 0; i<Member_ind.size(); i++){
-                if(!member[Member_ind[i]].force.done){
-                    undeter.push_back(Member_ind[i]);
-                }
-
-                else{
-                    deter.push_back(Member_ind[i]);
-                }
-            }
-
-            if(undeter.size() == 2){
-                member_forces(joint, support, member, force, j, deter, undeter);
-            }
-
-            else{
-                continue;
-            }
-
-        }
-    }
-
-}
-
-
-
 
 void gauss_jordan(double A[nDim][nDim], double B[nDim][1], double F[nDim][1]){
     
